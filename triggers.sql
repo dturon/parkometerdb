d59111d80d300dc34154a5b0aa0212af5d821c4a
David Turon <david.turon@linuxbox.cz>
2016-05-04 22:28:43 +0200

    get_size_parking function

--- triggers.sql
+++ triggers.sql
@@ -1,3 +1,10 @@
+CREATE OR REPLACE FUNCTION get_size_parking()
+RETURNS int AS
+$$
+    SELECT 5;
+$$ LANGUAGE SQL;
+
+
 CREATE OR REPLACE FUNCTION process_buffer(b buffer)
 RETURNS VOID AS
 $$
@@ -10,11 +17,12 @@ BEGIN
     SELECT car_id, parked, last_change FROM hub_parking WHERE car_id = ANY(b.proposed_car_ids) INTO _car_id, _parked, _last_change;
     IF NOT FOUND THEN
         --car probably isn't hub car 
+        RAISE DEBUG 'not found ts: %, filename: %', b.ts, b.filename; 
         RETURN;
     END IF;
 
     -- check if last change is more then 15s
-    IF _last_change IS NULL OR _last_change +'15s'::interval < now() THEN
+    IF _last_change IS NULL OR _last_change +'15s'::interval < clock_timestamp() THEN
         RAISE DEBUG 'found car_id: %, parked: %, updating to parked: %', _car_id, _parked, NOT _parked;       
         UPDATE hub_parking SET parked = NOT parked, last_change = b.ts WHERE car_id = _car_id;
     END IF;
@@ -23,7 +31,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE FUNCTION buffer_insert()
+CREATE OR REPLACE FUNCTION buffer_insert()
 RETURNS TRIGGER AS
 $$
 BEGIN
@@ -32,21 +40,21 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
+DROP TRIGGER IF EXISTS buffer_insert ON buffer; 
 CREATE TRIGGER buffer_insert 
 AFTER INSERT
 ON buffer
-FOR EACH ROW EXECUTE PROCEDURE buffer_insert();
+FOR EACH ROW 
+EXECUTE PROCEDURE buffer_insert();
 
 
 CREATE OR REPLACE FUNCTION hub_parking_insert_update_delete()
 RETURNS TRIGGER AS
 $$
 DECLARE
-    _park_size int DEFAULT 10;
     _full boolean;
 BEGIN
-    SELECT count(*) >= _park_size FROM hub_parking WHERE parked INTO _full;
+    SELECT count(*) >= get_size_parking() FROM hub_parking WHERE parked INTO _full;
     
     --let's notify semafor server 
     IF _full THEN
@@ -63,8 +71,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
+DROP TRIGGER IF EXISTS hub_parking_insert_update_delete ON hub_parking; 
 CREATE TRIGGER hub_parking_insert_update_delete
-AFTER INSERT OR UPDATE OR DELETE
+AFTER INSERT OR UPDATE OF parked OR DELETE
 ON hub_parking
-FOR EACH ROW EXECUTE PROCEDURE hub_parking_insert_update_delete();
+FOR EACH ROW 
+EXECUTE PROCEDURE hub_parking_insert_update_delete();
